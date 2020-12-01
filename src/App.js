7a70871bda3d9c31fa127aa265d35be7ce2006f2
Tak113 <takniques@gmail.com>
2020-11-30 21:44:35 -0700

    124 layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+import React, { Component } from 'react';
+
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// }
 
 export default App;
