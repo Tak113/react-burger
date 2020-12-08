07260f6792683ebab0754fb5f5b7e556e6389dc1
Tak113 <takniques@gmail.com>
2020-12-08 13:54:55 -0700

    update logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import MioLogo from '../../assets/images/burger-logo-Mio.png';
-import burgerLogo from '../../assets/images/burger-logo.png';
+// import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css'
 
 const logo = (props) => (
 	<div className={classes.Logo} style={{ height: props.height }}>
 		{/*<img src={MioLogo} alt="MioBurger" />*/}
-		<img src={burgerLogo} alt="MioBurger" className={classes.LogoBurger} />
+		<img src={MioLogo} alt="MioBurger" className={classes.LogoBurger} />
 	</div>
 );
 
